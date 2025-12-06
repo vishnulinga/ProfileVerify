0df6be80ce816847eb7fade89ad9bc415a3bdad4
Fitcomn2024 <Fitcomn2@gmail.com>
2025-12-06 17:44:56 -0600

    Initial MVP

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: 'dev-secret-key',
+    secret: process.env.SESSION_SECRET || 'dev-secret-key',
     resave: false,
     saveUninitialized: false
   })
